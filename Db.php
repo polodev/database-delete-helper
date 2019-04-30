8e814880da2debd6807d2e4b63020d049bfe2ede
polodev <polodev10@gmail.com>
2019-05-01 01:15:59 +0600

    added notification plus readme.md

--- Db.php
+++ Db.php
@@ -40,9 +40,14 @@ class Db {
 		foreach($arr as $table_name) {
 			$this->delete_single_table($table_name);
 		}
+		echo "All database delete successfully";
 	}
 	public function delete_all_tables_worker() {
 		$tables = $this->get_all_tables();
-		$this->delete_all_tables($tables);
+		if ($tables) {
+			$this->delete_all_tables($tables);
+		} else {
+			echo 'Dbs is empty. No Table found';
+		}
 	}
 }
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Database table delete helper 
+
+It will delete all your database recursively. To delete all wordpress table and install wordpress again I have wrote this. 
+
+Hope you will find it useful.
+
+
+## How to use it 
+
+Just keep this whole folder inside your server. Like `htdocs` in `xampp`, `www` in `wamp`. Then just visit the webpage and enter credentials.
+
+
+Thanks
